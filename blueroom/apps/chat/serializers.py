a77faeab0ec2f37702981c3ffc85a541f5f44e72
mie <nguyenthihuyenmy239@gmail.com>
2024-11-29 01:42:41 +0700

    Add some basic features to communicate in room

---
+++ blueroom/apps/chat/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Message
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = ['id', 'user', 'content', 'timestamp', 'type']
+
+    def get_user(self, obj):
+        return obj.participation_id.user_id.username
